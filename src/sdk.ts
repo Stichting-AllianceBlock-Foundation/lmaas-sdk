04611e9be97ba40c8ca082b4c5a8cd739ae80651
guillermoriv <guillermo.campanudo@gmail.com>
2022-08-08 09:12:01 +0100

    :art: adding utility class for the networks

--- src/sdk.ts
+++ src/sdk.ts
@@ -65,4 +65,33 @@ export class StakerSDK {
       getTokensConfig(config.tokens.filter(item => item.network === protocol)),
     );
   }
+
+  /**
+   *  Extract the chainId into a enum easy to select and have control.
+   *  @param {number} chainId - chainId where we can return the current network.
+   */
+  getProtocolByChainId(chainId: number) {
+    switch (chainId) {
+      case 1:
+      case 42:
+      case 3:
+      case 4:
+      case 5:
+        return NetworkEnum.eth;
+      case 56:
+        return NetworkEnum.bsc;
+      case 137:
+      case 80001:
+        return NetworkEnum.polygon;
+      case 43113:
+      case 43114:
+        return NetworkEnum.avalanche;
+      case 246:
+        return NetworkEnum.ewc;
+      case 1284:
+        return NetworkEnum.moonbeam;
+      default:
+        return NetworkEnum.localhost;
+    }
+  }
 }
