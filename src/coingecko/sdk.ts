890a10017573cdaaac7e88565e5e2328b4511c5b
guillermoriv <guillermo.campanudo@gmail.com>
2022-11-04 11:15:19 +0000

    :sparkles: Adding the coingecko rate-limits fix for the sdk

--- src/coingecko/sdk.ts
+++ src/coingecko/sdk.ts
@@ -3,6 +3,12 @@ import axios from 'axios';
 const coingeckoAPI = 'https://api.coingecko.com/api/v3';
 
 export class CoinGecko {
+  minutesForExpiration: number;
+
+  constructor(minutesToExpire: number) {
+    this.minutesForExpiration = minutesToExpire;
+  }
+
   /**
    *  Represents a class that can interact with DEX's
    *  depending on the network.
@@ -10,12 +16,54 @@ export class CoinGecko {
    *  @param {string} currency - Currency on which we want the price, ex: `usd`, `eur`.
    */
   async getTokenPrice(tokenId: string, currency: string) {
+    let usdPrices: { [key: string]: { [key: string]: number; expiration: number } } | null = null;
+    const secondsForExpiration = this.minutesForExpiration * 60;
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+
+    try {
+      usdPrices = JSON.parse(localStorage.getItem('usd_prices')!);
+    } catch (error) {
+      console.error(error);
+    }
+
+    if (usdPrices) {
+      if (usdPrices[tokenId] && usdPrices[tokenId].expiration > currentTimestamp) {
+        if (usdPrices[tokenId][currency]) {
+          return usdPrices[tokenId][currency];
+        }
+      }
+    }
+
     const response = await axios.get(coingeckoAPI + `/simple/price`, {
       params: {
         ids: tokenId,
         vs_currencies: currency,
       },
     });
+
+    if (usdPrices) {
+      usdPrices = {
+        ...usdPrices,
+        [tokenId]: {
+          [currency]: response.data[tokenId][currency],
+          expiration: currentTimestamp + secondsForExpiration,
+        },
+      };
+
+      localStorage.setItem('usd_prices', JSON.stringify(usdPrices));
+
+      return response.data[tokenId][currency];
+    }
+
+    usdPrices = {
+      [tokenId]: {
+        [currency]: response.data[tokenId][currency],
+        expiration: currentTimestamp + secondsForExpiration,
+      },
+    };
+
+    localStorage.setItem('usd_prices', JSON.stringify(usdPrices));
+
     return response.data[tokenId][currency];
   }
 }
